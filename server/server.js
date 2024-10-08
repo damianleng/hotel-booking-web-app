5884ffc5b5be7f8f829a4f6f4eb3344f90a3a351
damianlengdy <damianlengdy@gmail.com>
2024-10-07 20:52:49 -0500

    updated gitignore file

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
 const { fetchRooms } = require("./fetch_service/roomService");
 const { fetchUsers } = require("./fetch_service/userService");
 const { fetchBooking } = require("./fetch_service/bookingService");
@@ -6,13 +8,18 @@ const { fetchDigitalkey } = require("./fetch_service/digitalkeyService");
 const { fetchHousekeeping } = require("./fetch_service/housekeepingService");
 const { fetchPayment } = require("./fetch_service/paymentService");
 
-// Use .env file to store sensitive data
-const mongoUri = `mongodb+srv://admin:mIgakKO2YvR7AiOc@hotelkeeping.9aykv.mongodb.net/hotelkeeping?retryWrites=true&w=majority&appName=hotelkeeping`; // Single database for all collections
+dotenv.config({ path: "./config.env" });
 
-mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
+// connect to database using dotenv
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(async () => {
     console.log("Connected to Hotelkeeping Database");
-
     // Fetch data
     await fetchRooms();
     await fetchUsers();
