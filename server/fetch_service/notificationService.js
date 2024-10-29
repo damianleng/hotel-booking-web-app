42b009f965d329add6441b730b8420d4539d2fae
chhay serey vath <chhaysereyvath3112@gmail.com>
2024-10-29 13:28:59 -0500

    finishing email HTML

--- server/fetch_service/notificationService.js
+++ server/fetch_service/notificationService.js
@@ -6,25 +6,25 @@ const transporter = nodemailer.createTransport({
   service: "Gmail", // use Gmail as the mail service
   auth: {
     user: process.env.EMAIL, // Gmail address
-    pass: process.env.APP_PASSWORD, // APP PASSWORD
+    pass: process.env.APP_PASSWORD, // App-specific password
   },
 });
 
-// Function to send an email notification
-exports.sendEmailNotification = async (to, subject, message) => {
+// Function to send an email notification with HTML support
+exports.sendEmailNotification = async (to, subject, htmlMessage) => {
   try {
     // prepare to send message
     const mailOptions = {
       from: process.env.EMAIL,
       to: to,
       subject: subject,
-      text: message,
+      html: htmlMessage, // Use 'html' instead of 'text' for HTML content
     };
 
     // send the message
     await transporter.sendMail(mailOptions);
     console.log("Email sent successfully!");
   } catch (error) {
-    console.error("Error sending email: ", error);
+    console.error("Error sending email:", error);
   }
 };
