3192d9f8e28cc8e9ae52ecaba813cabf7c020eaa
chhay serey vath <chhaysereyvath3112@gmail.com>
2024-10-29 20:23:52 -0500

    fixed payment mobile

--- client/src/app/components/payment/payment.component.ts
+++ client/src/app/components/payment/payment.component.ts
@@ -7,6 +7,7 @@ import { EventEmitter } from "@angular/core";
 @Component({
   selector: "app-payment",
   templateUrl: "./payment.component.html",
+  styleUrls: ["./payment.component.css"],
 })
 export class PaymentComponent implements OnInit {
   @Input() totalPrice: number = 0;
