d9f3b78462439099b1e91a95ccb4397f1f715c5b
damianlengdy <damianlengdy@gmail.com>
2025-07-21 17:02:40 +0700

    fixed stripe key not working

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  stripePublicKey: (window as any)["env"]["stripePublicKey"],
+  stripePublicKey: "pk_test_51Q7q4BRumJvqTRUCrPwt5z8AWHyT5F2gBiwsmfbAXfVxrFzxdjtI7Jph40TNmFJxOusSAHLxZT2iHXQ1ONUDFDjf002BRQlQmV",
   apiUrl: "https://aurora-backend-hsrg.onrender.com/api",
 };
